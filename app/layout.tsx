4c7e05fbfa921df90226249f3b1a61b547e4378c
Saurabh Yadav <Saurabhyadav831@gmail.com>
2025-03-20 03:49:08 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NapCart",
   description: "NapCart Logo Particles",
+  icons: {
+    icon: "/favicon.ico",
+    }
 };
 
 export default function RootLayout({
